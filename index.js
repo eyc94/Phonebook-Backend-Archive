c84eb8bc6e6bec3571979c436557db1d323a9e81
Eric Chin <ericychin94@gmail.com>
2021-10-04 05:58:19 -0700

    Added promise chaining

--- index.js
+++ index.js
@@ -81,8 +81,9 @@ app.post('/api/persons', (request, response, next) => {
 
     person
         .save()
-        .then(savedPerson => {
-            response.json(savedPerson.toJSON())
+        .then(savedPerson => savedPerson.toJSON())
+        .then(savedAndFormattedPerson => {
+            response.json(savedAndFormattedPerson)
         })
         .catch(error => next(error))
 })
